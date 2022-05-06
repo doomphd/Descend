6286ae52c1529d82c15e2659f882e470da193153
Truman Tang <trumantang99@gmail.com>
2022-05-05 23:29:44 -0700

    finish

---
+++ Descend/Assets/Combat Scene/scripts/Enemy/Feather.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feather : MonoBehaviour
+{
+
+    public int damage = 1;
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    void Start(){
+        rb.velocity = -transform.right *  speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo){
+        
+        PlayerController player = hitInfo.GetComponent<PlayerController>();
+        if(player != null)
+        {
+            player.TakeHit(damage);
+            Destroy(gameObject);
+        }
+        StartCoroutine(Wait());
+        
+
+        
+    }
+        private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+
+}
