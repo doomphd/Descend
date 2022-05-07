f493e528cdd23efe08b9a558c81924185042b21a
adele-wu <adelewu2000@gmail.com>
2022-05-06 22:40:56 -0700

    make 'solo/multiplayer' panel on the same scene as the title panel

--- Descend/Assets/Visuals/scripts/PanelOpener.cs
+++ Descend/Assets/Visuals/scripts/PanelOpener.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanelOpener : MonoBehaviour 
+public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
+    public GameObject Panel_Current;
 
     public void OpenPanel()
     {
-        if(Panel != null)
+        if (Panel != null)
         {
             bool isActive = Panel.activeSelf;
             Panel.SetActive(!isActive);
+        }
+
+        if (Panel_Current != null)
+        {
+            bool isActive2 = Panel_Current.activeSelf;
+            Panel_Current.SetActive(!isActive2);
         }
     }
 }
